ab15e0389eebd01f41265b7e2de4c335fab90220
Kirill <kirillnaumenk0@mail.ru>
2022-09-27 14:29:35 +0600

    commit end

--- №2.py
+++ №2.py
@@ -4,5 +4,5 @@ print('x y z')
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            if (not(x or y or z) == (not x) and (not y) and (not z)):
+            if not (x or y or z) == (not x and not y and not z):
                 print(x, y, z)
--- №4.py
+++ №4.py
@@ -2,3 +2,16 @@
 #показывает диапазон возможных координат точек в этой четверти (x и y).
 #- A (3,6); B (2,1) -> 5,09
 #- A (7,-5); B (1,-1) -> 7,21
+
+quarter = int(input())
+
+if quarter == 1:
+    print("x > 0, y > 0")
+elif quarter == 2:
+    print("x < 0, y > 0")
+elif quarter == 3:
+    print("x < 0, y < 0")
+elif quarter == 4:
+    print("x > 0, y < 0")
+else:
+    print("Неверный номер")
